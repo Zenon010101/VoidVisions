f790f340c1be317d03d354ae7a61ff1d142daf78
Zenon_01 <dumpryzmc@gmail.com>
2025-02-02 19:22:20 +0800

    Add files via upload

--- navigation.js
+++ navigation.js
@@ -1,16 +1,20 @@
 // define page content
 const pages = {
     home: `
+    <div class="container"><div class="line">
+        
+    </div></div>
         <center>
+        <br>
             <h1>Welcome to</h1>
-             <img src="VOID VISUALS.png" usemap="#VoidVision" alt="VoidVision">
+             <img class="a" src="VOID VISUALS.png" usemap="#VoidVision" alt="VoidVision">
         </center>
         <br> 
         <center>
         <h1> This is VOID</h1>
         </center>
         <center>
-        <img src="VoidVision Logo.png" usemap="#VoidVision" alt="VoidVision">
+        <img class="b" src="Void.png" usemap="#VoidVision" alt="VoidVision">
         </center>
        
         
@@ -34,12 +38,22 @@ const pages = {
             <br>
             <h1> This is how VoidVisuals was created. </h1>
             <br>
-            <p> If you are interested, please watch this video. </p>
-             <video width="500" height="250" controls>
+            
+            <p> If you are interested, watch the video. </p>
+            <video width="500" height="250" controls>
             <source src="THE CREATION OF VOID VISION.mp4" type="video/mp4">
             Your browser does not support the video tag.
+
+           
         </video>
+
+        
         </center>
+        <h1>Visit my links</h1>
+        <a href="https://www.facebook.com/VoidVisionsPh"><img class="Facebook" src="fb.png" alt="Facebook" style="width:42px;height:42px;"> </a> 
+        <br>
+        <br>
+        <a href="https://www.instagram.com/voidvisionsph?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="><img class="Instagram" src="ig.png" alt="Instagram" style="width:42px;height:42px;"> </a> 
     `
 };
 
